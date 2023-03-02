b8228ee3eefb28d87f1d51b9de06f9caada9443b
Yoonsin <zpssol3@naver.com>
2023-03-02 14:19:19 +0900

    17404_re, 2225_re Update
    알고리즘 기초 1/2 https://code.plus/course/41
    문제 다풀었다!!!!!

--- Dynamic Algorithm_1/Dynamic Algorithm_1/17404_re.cpp
+++ Dynamic Algorithm_1/Dynamic Algorithm_1/17404_re.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #define MAX 1000
+#define MAX_NUM 1000001
 using namespace std;
 
-//https://cocoon1787.tistory.com/498 Âü°í
 int arr[MAX + 1][3];
 int dp[MAX + 1][3];
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	int n;
-	int answer = 1000001;
 	cin >> n;
+
+	int answer = MAX_NUM;
 
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
@@ -17,20 +19,27 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			if (i == j) dp[1][j] = arr[1][j];
-			else dp[1][j] = 1000001;
+			else dp[1][j] = MAX + 1;
 		}
 
 		for (int j = 2; j <= n; j++) {
 			dp[j][0] = arr[j][0] + min(dp[j - 1][1], dp[j - 1][2]);
 			dp[j][1] = arr[j][1] + min(dp[j - 1][0], dp[j - 1][2]);
-			dp[j][2] = arr[j][2] + min(dp[j - 1][1], dp[j - 1][0]);
+			dp[j][2] = arr[j][2] + min(dp[j - 1][0], dp[j - 1][1]);
 		}
 
 		for (int j = 0; j < 3; j++) {
-			if (i != j) {
+			if (i == j)continue;
+			else {
 				answer = min(answer, dp[n][j]);
+				
 			}
+			
 		}
 	}
+
+	cout << answer;
+
+
 	return 0;
 }
--- Dynamic Algorithm_1/Dynamic Algorithm_1/2225_re.cpp
+++ Dynamic Algorithm_1/Dynamic Algorithm_1/2225_re.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
-#define MAX 201
+#define MAX 200
 #define Moduler 1000000000
 using namespace std;
 
-long long dp[MAX][MAX]; //dp[k][n]
-int main(int argc, char** argv) {
+int dp[MAX + 1][MAX + 1]; //dp[n][k]  0~n까지 k개를 더해서 합이 n이 되는 경우
+int main(int argc, char** argv)
+{
+	int n, k;
+	cin >> n >> k;
 
-	int N, K;
-	cin >> N >> K;
-
-	for (int i = 0; i <= N; i++) {
-		dp[1][i] = 1;
+	//초기화
+	for (int i = 0; i <= n; i++) {
+		dp[i][1] = 1;
 	}
 
-	for (int k = 2; k <= K; k++) {
-		for (int n = 0; n <= N; n++) {
-			for (int i = 0; i <= n; i++) {
-				dp[k][n] = dp[k][n] + dp[k - 1][i];
+	for (int i = 0; i <= k; i++) {
+		dp[0][i] = 1;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 2; j <= k; j++) {
+			for (int x = 0; x <= i; x++) {
+				dp[i][j] = (dp[i][j] +dp[x][j - 1])%Moduler;
 			}
-			dp[k][n] = dp[k][n] % Moduler;
 		}
 	}
-	
-	cout << dp[K][N] << '\n';
+
+	cout << dp[n][k] << '\n';
 
 	return 0;
 }
